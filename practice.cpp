d00c9067ed74b2778a3c2e20d9f66d85311a201e
Will <twitchifygamers@gmail.com>
2022-05-12 08:35:44 +0100

    Implemented the random order practice section

--- practice.cpp
+++ practice.cpp
@@ -2,9 +2,13 @@
 // Created by will on 11/05/22.
 //
 
+#include <algorithm>
+#include <fstream>
+#include <random>
 #include "practice.h"
 #include "iostream"
 #include "filesystem"
+#include "vector"
 
 using namespace practice;
 
@@ -32,5 +36,19 @@ void practice::menu()
 
 void practice::randomorder()
 {
-
+    // Randomly select a .txt file from the directory and output the first line of the file.
+    std::vector<std::string> files;
+    std::string path = "../flashcards/";
+    for (auto& entry : std::filesystem::directory_iterator(path)) {
+        if (entry.path().extension() == ".txt") {
+            files.push_back(entry.path().string());
+        }
+    }
+    std::shuffle(files.begin(), files.end(), std::mt19937(std::random_device()()));
+    std::cout << "\033[1;31m" << files[0] << "\033[0m" << std::endl;
+    std::ifstream file(files[0]);
+    std::string line;
+    std::getline(file, line);
+    std::cout << line << std::endl;
+    file.close();
 }
